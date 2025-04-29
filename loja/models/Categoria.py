99f8f2079ef01ec91c2c52f099fca46937eb3b4f
beawxy2410 <jb.beatriz48@gmail.com>
2025-04-29 15:31:52 -0300

    3 capítulo finalizado

--- loja/models/Categoria.py
+++ loja/models/Categoria.py
@@ -3,5 +3,5 @@ class Categoria(models.Model):
     Categoria = models.CharField(null=False, max_length=100)
     criado_em = models.DateTimeField(auto_now_add=True)
     alterado_em = models.DateTimeField(auto_now=True)
-def __str__(self):
-    return '{}'.format(self.Categoria)
+    def __str__(self):
+        return '{}'.format(self.Categoria)
