226f6e99f77ae180cf7fb740f89e2b6bd54192b5
beawxy2410 <jb.beatriz48@gmail.com>
2025-05-19 14:30:20 -0300

    filtros

--- loja/urls/ProdutoUrls.py
+++ loja/urls/ProdutoUrls.py
@@ -1,7 +1,8 @@
+from math import prod
 from django.urls import path
 from loja.views.ProdutoView import list_produto_view
 
 urlpatterns = [
-    path("", list_produto_view),
-    path("<int:id>", list_produto_view),
+    path("", list_produto_view, name="produtos"),
+    path("<int:id>", list_produto_view, name="produto"),
 ]
