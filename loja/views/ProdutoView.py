ba4219bc279d71ad91b8688494b60bc067d9ac9c
beawxy2410 <jb.beatriz48@gmail.com>
2025-06-10 15:59:38 -0300

    continuando o cap 7

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,8 +1,10 @@
 from datetime import datetime
-from django.http import HttpResponse
+from django.shortcuts import render
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
+
+
 
 #Carrega dados no navegador
 def list_produto_view(request, id=None):
@@ -30,9 +32,17 @@ def list_produto_view(request, id=None):
         now = timezone.now()
         now = now - timedelta(days = int(dias))
         produtos = produtos.filter(criado_em__gte=now)
+    
 
-    print(produtos)
+    context = { 'produtos': produtos }
+    return render(request, template_name='produto/produto.html', context=context,status=200)
 
-    if id is None:
-        return HttpResponse('<h1>Nenhum id foi informado</h1>')
-    return HttpResponse("<h1>Produto de id %s!</h1>" % id)
+
+def edit_produto_view(request, id=None):
+    produtos = Produto.objects.all()
+    if id is not None:
+        produtos = produtos.filter(id=id)
+    produto = produtos.first()
+    print(produto)
+    context = { 'produto': produto }
+    return render(request, template_name='produto/produto-edit.html', context=context,status=200)
