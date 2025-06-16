6398eda7c546c8dc35a3f1346b27faed6d4a9231
beawxy2410 <jb.beatriz48@gmail.com>
2025-06-16 14:18:58 -0300

    capitulo 7

--- loja/views/ProdutoView.py
+++ loja/views/ProdutoView.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from loja.models import Produto
 from datetime import timedelta, datetime
 from django.utils import timezone
@@ -37,6 +37,32 @@ def list_produto_view(request, id=None):
     context = { 'produtos': produtos }
     return render(request, template_name='produto/produto.html', context=context,status=200)
 
+def edit_produto_postback(request, id=None):
+    if request.method == 'POST':
+        # Salva dados editados
+        id = request.POST.get("id")
+        produto = request.POST.get("Produto")
+        destaque = request.POST.get("destaque")
+        promocao = request.POST.get("promocao")
+        msgPromocao = request.POST.get("msgPromocao")
+        print("postback")
+        print(id)
+        print(produto)
+        print(destaque)
+        print(promocao)
+        print(msgPromocao)
+        try:
+            obj_produto = Produto.objects.filter(id=id).first()
+            obj_produto.Produto = produto
+            obj_produto.destaque = (destaque is not None)
+            obj_produto.promocao = (promocao is not None)
+            if msgPromocao is not None:
+                obj_produto.msgPromocao = msgPromocao
+                obj_produto.save()
+            print("Produto %s salvo com sucesso" % produto)
+        except Exception as e:
+            print("Erro salvando edição de produto: %s" % e)
+    return redirect("/produto")
 
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
